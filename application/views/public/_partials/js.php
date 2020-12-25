62a6b91ae4726aba6424d138ee39798baf84c2ab
syarif taufik <syariftaufik11@gmail.com>
2020-12-25 19:06:22 +0700

    setup awal menambah template tampilan frontend dan template backend

---
+++ application/views/public/_partials/js.php
@@ -0,0 +1,5 @@
+<script src="<?php echo base_url('assets/vendor/jquery/jquery.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+
+<!-- Custom scripts for this template -->
+<script src="<?php echo base_url('assets/js/clean-blog.min.js') ?>"></script>
